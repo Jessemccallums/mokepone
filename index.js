aceca829ccd64d004fe641590f4720e8fb776321
JesseMcCallums <jessemccallums2001@gmail.com>
2022-08-15 08:13:23 -0400

    Update js file

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+function iniciarJuego(){
+
 let botonSeleccionar = document.getElementById('boton-mascota')
 botonSeleccionar.addEventListener('click', seleccionarPersonajeJugador)
-
-
+let ocultarSeleccionarPersonaje = document.getElementById("seleccionar-ataque")
+ocultarSeleccionarPersonaje.style.display = "none";
+}
 function seleccionarPersonajeJugador() {
     let seleccionarHipodoge = document.getElementById("hipodoge");
     let seleccionarCapipepo = document.getElementById("capipepo");
     let seleccionarRatigueya = document.getElementById("ratigueya");
+    let ocultarSeleccionarPersonaje = document.getElementById("seleccionar-ataque")
+    ocultarSeleccionarPersonaje.style.display = "block";
+
 
     if (seleccionarHipodoge.checked) {
         let mascotaSeleccionada = document.getElementById("mascotaSeleccionadaJugador")
@@ -17,9 +23,9 @@ function seleccionarPersonajeJugador() {
     } else if (seleccionarRatigueya.checked) {
         let mascotaSeleccionada = document.getElementById("mascotaSeleccionadaJugador")
         mascotaSeleccionada.innerHTML = "Ratigueya"
-    } else {
-        alert("Selecciona tu mascota!")
-    }
+      } 
+
+   
 
     seleccionarPersonajeEnemigo()
 
@@ -28,7 +34,7 @@ function seleccionarPersonajeJugador() {
 function seleccionarPersonajeEnemigo() {
     let ataqueAleatorio = aleatorio(1, 3)
     let mascotaSeleccionadaEnemigo = document.getElementById("mascotaSeleccionadaEnemigo");
-
+   
     if (ataqueAleatorio == 1) {
         mascotaSeleccionadaEnemigo.innerHTML = "Hipodoge"
     } else if (ataqueAleatorio == 2) {
@@ -37,11 +43,17 @@ function seleccionarPersonajeEnemigo() {
         mascotaSeleccionadaEnemigo.innerHTML = "Ratigueya"
     }
 
+    
+    seleccionarPersonajeJugador()
+
 }
 
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+
+window.addEventListener('load', iniciarJuego)
 
 seleccionarPersonajeJugador()
 
